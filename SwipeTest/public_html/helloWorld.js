307f5ab993df2de4de34a6e8732ccb33f96412d2
Ashley <ablondewell@gmail.com>
2014-01-30 17:59:21 -0800

    Basic Grid Functionality

--- SwipeTest/public_html/helloWorld.js
+++ SwipeTest/public_html/helloWorld.js
@@ -3,21 +3,41 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+document.write('<h1>Laser Gate</h1><table border="1" cellspacing = "1" cellpadding = "5" id="a">');
+
+for (i = 0; i < 9; i++) {
+    document.write("<tr id='row" + i + "'>'");
+    for (j = 0; j < 9; j++) {
+        if (j == 0 | j == 8) {
+            document.write("<td id= '" + i.toString() + j.toString() + "' class='outer'> row " + i + " col " + j + "</td>");
+        }
+        else {
+            document.write("<td id= '" + i.toString() + j.toString() + "'> row " + i + " col " + j + "</td>");
+        }
+    }
+    document.write('</tr>');
+}
+$("#row0").addClass("outer");
+$("#row8").addClass("outer");
+
+document.write('</table>');
+
 $(function() {
     //Enable swiping...
-    $("div.box").swipe({
+    $("table").swipe({
         //Generic swipe handler for all directions
         swipe: function(event, direction, distance, duration, fingerCount) {
-            $('#hello').text("You swiped " + direction + " for " + distance + " pixels");
+            $('#grid').text("You swiped " + direction + " for " + distance + " pixels");
         },
         //Default is 75px, set to 0 for demo so any distance triggers swipe
         threshold: 0
     });
+    $("table").on("click", function() {
+        alert("Level 1");
+        $("#00").text("laser").addClass("laser");
+        $("#60").text("laser").addClass("laser");
+        $("#38").text("laser").addClass("laser");
+    });
+    
 });
-
-function draw(width, height, cols, rows) {
-    var y = height/rows;
-    var x = width/cols;
-    var i=y;
-   
-}
